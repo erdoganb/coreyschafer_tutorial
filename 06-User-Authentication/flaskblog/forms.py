0674940c4b6f3fc69d7d3b8acfdb640f4242845c
erdoganb <erdoganborklu@gmail.com>
2023-04-02 19:43:57 +0300

    new update - following the tutorial

---
+++ 06-User-Authentication/flaskblog/forms.py
@@ -0,0 +1,30 @@
+# forms for register and login
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])  # password must be the name of the password, not 'Password' above
+    submit = SubmitField('Sign Up!')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already taken, try a new one!')
+
+    def validate_email(self, email):
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('Email is in use, try a new one!')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In!')
